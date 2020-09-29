5115b7824d9978e65321c080ab065b3b73d84efc
Gouri Shanker <shanker.gouri@hotmail.com>
2020-09-29 17:53:26 +0530

    Selection Sort

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,84 +1,83 @@
+function sleep(delay) {
+	return new Promise(resolve => setTimeout(resolve, delay));
+}
 var i = 0;
-	while (i < 7) {
-		document.querySelector("#height0").style.height = "195px"
-		var tree = document.querySelector("#height0").cloneNode(true);
-		if (i <=3){
-			tree.style.height = (i+1)*100 +"px";
+while (i < 15) {
+	document.querySelector("#height0").style.height = "195px"
+	var tree = document.querySelector("#height0").cloneNode(true);
+	tree.style.height = (Math.floor(Math.random() * 250) + 100) + "px";
+	tree.id = "height" + (i + 1);
+	tree.class = "heights";
+	document.querySelector("#Gouri").append(tree);
+	i += 1;
 }
-else {
-	tree.style.height = (i+1)*35 +"px";
+var selection_sort= document.querySelector("#bubble_sort").cloneNode(true);
+ document.querySelector("#bubble_sort p").innerHTML = "<h1>Bubble Sort Algorithm !</h2>"
+ selection_sort.querySelector("p").innerHTML = "<h1>Selection Sort Algorithm !</h2>"
+selection_sort.id = "selection_sort"
+selection_sort.querySelector("#lol").id="lol1"
+selection_sort.querySelector("#Gouri").id="Gouri1"
+document.querySelector("#main").append(selection_sort);
+console.log(selection_sort)
+
+
+async function bubblesort() {
+	var n = document.querySelector("#Gouri").childElementCount
+	var rectangles = document.querySelectorAll(".heights")
+	for (i = 0; i < n - 1; i++) {
+		for (var j = i; j < n; j++) {
+			rectangles[i].style.backgroundColor = "white"
+			rectangles[j].style.backgroundColor = "white"
+			await sleep(150)
+			let first = Number(rectangles[i].style.height.split("p")[0])
+			let second = Number(rectangles[j].style.height.split("p")[0])
+			if (first > second) {
+				rectangles[i].style.backgroundColor = "brown"
+				rectangles[j].style.backgroundColor = "brown"
+				await sleep(150)
+				rectangles[i].style.height = second + "px"
+				rectangles[j].style.height = first + "px"
+			}
+			rectangles[i].style.backgroundColor = "lightgreen"
+			rectangles[j].style.backgroundColor = "lightgreen"
+			await sleep(150)
+
+		}
+		rectangles[i].style.backgroundColor = "green"
+		await sleep(150)
+	}
+	rectangles[n - 1].style.backgroundColor = "green"
 }
-		tree.id = "height" + (i+1)
-		tree.class = "heights";
-		document.querySelector("#Gouri").append(tree);
-		i += 1;
-	}
+async function selection_sorting() {
+	var n = document.querySelector("#Gouri1").childElementCount
+	console.log(n)
+	var rectangles = document.querySelectorAll("#Gouri1 .heights")
+	console.log(rectangles)
+	var min = i
+	for (i = 0; i < n - 1; i++) {
+		for (var j = i+1; j < n; j++) {
+			let first = Number(rectangles[j].style.height.split("p")[0])
+			let second = Number(rectangles[min].style.height.split("p")[0])
+			rectangles[j].style.backgroundColor = "white"
+			await sleep(150)
+			if (second > first) {
 
-	function sleep(delay){
-	    return new Promise(resolve=>setTimeout(resolve,delay));
-	}
-
-
-async function bubblesort(){
-var n = document.querySelector("#Gouri").childElementCount
-var rectangles = 	document.querySelectorAll(".heights")
-for(i = 0 ;i<n-1 ;i++){
-	 for (var j =i ;j<n ;j++){
-		 rectangles[i].style.backgroundColor = "white"
-		 rectangles[j].style.backgroundColor = "white"
-		 await sleep(300)
-		 let first = Number(rectangles[i].style.height.split("p")[0])
-		 let second = Number(rectangles[j].style.height.split("p")[0])
-		 if (first > second ){
-			 rectangles[i].style.backgroundColor = "brown"
-			 rectangles[j].style.backgroundColor = "brown"
-			 await sleep(250)
-			 rectangles[i].style.height = second +"px"
-			 rectangles[j].style.height = first+"px"
-		 }
-		 rectangles[i].style.backgroundColor = "lightgreen"
-		 rectangles[j].style.backgroundColor = "lightgreen"
-		 await sleep(250)
-
-	 }
-	 rectangles[i].style.backgroundColor = "green"
-	 await sleep(250)
- }
- rectangles[n-1].style.backgroundColor = "green"
- }
-// console.log(Number(i.style.height.split("p")[0]))
-
-
-
-//
-// i = 0
-// var heights = []
-// for (i = 1 ; i< 7 ; i++){
-// 	  var s = "#height"+i;
-// 	    var heightpx = document.querySelector(s).style.height
-// 			var height = parseInt(heightpx.split("p")[0])
-// 			heights.push(height)
-//
-// }
-// function bubblesort(){
-// var j = 0
-// for (i = 0 ;i< heights.length-1 ;i++){
-// 	for ( j = i+1 ;j<heights.length;j++){
-// 	if (heights[i] > heights[j]){
-// 		let temp;
-// 		var s = "#height"+(i+1);
-// 			var d = "#height"+(j+1);
-// 			console.log(s)
-// 			console.log(d)
-// 			console.log(heights[i])
-//     document.querySelector(s).style.height = heights[j]+"px"
-// 		document.querySelector(d).style.height = heights[i]+"px"
-// temp = heights[i];
-// heights[i] = heights[j];
-// heights[j] = temp;
-//
-// }
-// 	}
-// }
-// }
-document.querySelector("#lol").addEventListener("click" , bubblesort);
+			     min = j
+			}
+			 rectangles[j].style.backgroundColor = "lightgreen"
+		}
+    if (min !=i){
+			rectangles[i].style.backgroundColor = "brown"
+			rectangles[min].style.backgroundColor = "brown"
+			await sleep(250)
+			let temp = rectangles[i].style.height
+			rectangles[i].style.height = rectangles[min].style.height
+			rectangles[min].style.height = temp
+		}
+		rectangles[i].style.backgroundColor = "green"
+ 	 await sleep(250)
+}
+	rectangles[n-1].style.backgroundColor = "green"
+}
+document.querySelector("#lol").addEventListener("click", bubblesort);
+document.querySelector("#lol1").addEventListener("click", selection_sorting);
